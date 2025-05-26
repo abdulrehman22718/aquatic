3f6a356a47881b05371fa085baf711d305aa9558
abdulrehman22718 <abdulrehman22718@gmail.com>
2025-05-26 16:57:51 +0500

    Working on Navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,67 +4,68 @@ import { Link } from 'react-router-dom';
 const Header = ({ onToggleMenu }) => {
     return (
         <>
-                    <header className="th-header header-layout3 header-absolute onepage-nav">
-                        <div className="header-top">
-                            <div className="container th-container">
-                                <div className="row justify-content-center justify-content-lg-between align-items-center gy-2">
-                                    <div className="col-auto d-none d-lg-block">
-                                        <div className="header-links">
-                                            <ul>
-                                                <li>
-                                                    <i className="far fa-phone"></i>
-                                                    <a href="tel:+46825476243">(+468) 254 76243</a>
-                                                </li>
-                                                <li>
-                                                    <i className="fa-regular fa-messages"></i>
-                                                    <a href="mailto:info@poolax.com">info@poolax.com</a>
-                                                </li>
-                                                <li>
-                                                    <i className="fa-light fa-clock"></i>Work Time: Mon - Fri 09AM - 6PM
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <div className="social-links">
-                                            <span className="social-title">Follow Us On:</span>
-                                            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-                                                <i className="fab fa-facebook-f"></i>
-                                            </a>
-                                            <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
-                                                <i className="fab fa-twitter"></i>
-                                            </a>
-                                            <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
-                                                <i className="fab fa-linkedin-in"></i>
-                                            </a>
-                                            <a href="https://www.skype.com/" target="_blank" rel="noreferrer">
-                                                <i className="fa-brands fa-skype"></i>
-                                            </a>
-                                            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
-                                                <i className="fab fa-youtube"></i>
-                                            </a>
-                                        </div>
-                                    </div>
+            <header className="th-header header-layout3 header-absolute onepage-nav">
+                <div className="header-top">
+                    <div className="container th-container">
+                        <div className="row justify-content-center justify-content-lg-between align-items-center gy-2">
+                            <div className="col-auto d-none d-lg-block">
+                                <div className="header-links">
+                                    <ul>
+                                        <li>
+                                            <i className="far fa-phone" aria-hidden="true"></i>
+                                            <a href="tel:+18436058966">(843) 605-8966</a>
+                                        </li>
+
+                                        <li>
+                                            <i className="fa-regular fa-messages"></i>
+                                            <a href="mailto:info@poolax.com">info@example.com</a>
+                                        </li>
+                                        <li>
+                                            <i className="fa-light fa-clock"></i>Work Time: Mon - Fri 09AM - 6PM
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-auto">
+                                <div className="social-links">
+                                    <span className="social-title">Follow Us On:</span>
+                                    <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                                        <i className="fab fa-facebook-f"></i>
+                                    </a>
+                                    <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
+                                        <i className="fab fa-twitter"></i>
+                                    </a>
+                                    <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+                                        <i className="fab fa-linkedin-in"></i>
+                                    </a>
+                                    <a href="https://www.skype.com/" target="_blank" rel="noreferrer">
+                                        <i className="fa-brands fa-skype"></i>
+                                    </a>
+                                    <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+                                        <i className="fab fa-youtube"></i>
+                                    </a>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
 
-                        {/* Main Menu */}
-                        <div className="sticky-wrapper">
-                            <div className="menu-area">
-                                <div className="container th-container">
-                                    <div className="row align-items-center justify-content-between">
-                                        <div className="col-auto">
-                                            <div className="header-logo">
-                                                <Link to="/">
-                                                    <img src="assets/img/logo3.svg" alt="Poolax" />
-                                                </Link>
-                                            </div>
-                                        </div>
-                                        <div className="col-auto me-lg-auto">
-                                            <nav className="main-menu d-none d-lg-block">
-                                                <ul>
-                                                    {/* <li className="menu-item-has-children">
+                {/* Main Menu */}
+                <div className="sticky-wrapper">
+                    <div className="menu-area">
+                        <div className="container th-container">
+                            <div className="row align-items-center justify-content-between">
+                                <div className="col-auto">
+                                    <div className="header-logo">
+                                        <Link to="/">
+                                            <img src="assets/img/logo3.svg" alt="Poolax" />
+                                        </Link>
+                                    </div>
+                                </div>
+                                <div className="col-auto me-lg-auto">
+                                    <nav className="main-menu d-none d-lg-block">
+                                        <ul>
+                                            {/* <li className="menu-item-has-children">
                                                 <a href="index.html">Home</a>
                                                 <ul className="sub-menu">
                                                     <li className="menu-item-has-children">
@@ -97,13 +98,32 @@ const Header = ({ onToggleMenu }) => {
                                                     </li>
                                                 </ul>
                                             </li> */}
+                                            <li>
+                                                <Link to="/">Home</Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/About-Us">About Us</Link>
+                                            </li>
+                                            <li className="menu-item-has-children">
+                                                <a href="#">Services</a>
+                                                <ul className="sub-menu">
                                                     <li>
-                                                        <Link to="/">Home</Link>
+                                                        <a href="#">Pebble Tec Plaster</a>
                                                     </li>
                                                     <li>
-                                                        <Link to="/About-Us">About Us</Link>
+                                                        <a href="#">Concrete Pools</a>
                                                     </li>
-                                                    {/* <li>
+                                                    <li>
+                                                        <a href="#">Fiberglass Pools</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="#">Leak & Repair</a>
+                                                    </li>
+                                                   
+                                                </ul>
+                                            </li>
+
+                                            {/* <li>
                                                         <a href="#service-sec">Services</a>
                                                     </li>
                                                     <li>
@@ -115,38 +135,38 @@ const Header = ({ onToggleMenu }) => {
                                                     <li>
                                                         <a href="#contact-sec">Contact Us</a>
                                                     </li> */}
-                                                </ul>
-                                            </nav>
-                                        </div>
-                                        <div className="col-auto">
-                                            <div className="header-button">
-                                                <button type="button" className="icon-btn searchBoxToggler">
-                                                    <i className="far fa-search"></i>
-                                                </button>
-                                                <a
-                                                    href="#"
-                                                    className="icon-btn sideMenuToggler d-none d-lg-block"
-                                                    aria-label="Open Side Menu"
-                                                >
-                                                    <i className="far fa-bars"></i>
-                                                </a>
-                                                 <Link to="/About-Us" className="request-quote">
-                                                    Request a quote
-                                                </Link>
-                                                <button
-                                                    className="icon-btn th-menu-toggle d-inline-block d-lg-none"
-                                                    aria-label="Toggle Menu"
-                                                    onClick={onToggleMenu}
-                                                >
-                                                    <i className="far fa-bars"></i>
-                                                </button>
-                                            </div>
-                                        </div>
+                                        </ul>
+                                    </nav>
+                                </div>
+                                <div className="col-auto">
+                                    <div className="header-button">
+                                        <button type="button" className="icon-btn searchBoxToggler">
+                                            <i className="far fa-search"></i>
+                                        </button>
+                                        <a
+                                            href="#"
+                                            className="icon-btn sideMenuToggler d-none d-lg-block"
+                                            aria-label="Open Side Menu"
+                                        >
+                                            <i className="far fa-bars"></i>
+                                        </a>
+                                        <Link to="/About-Us" className="request-quote">
+                                            Request a quote
+                                        </Link>
+                                        <button
+                                            className="icon-btn th-menu-toggle d-inline-block d-lg-none"
+                                            aria-label="Toggle Menu"
+                                            onClick={onToggleMenu}
+                                        >
+                                            <i className="far fa-bars"></i>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </header>
+                    </div>
+                </div>
+            </header>
 
         </>
     );
---
+++ src/Components/SideNav.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+function SideNav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+
+  );
+}
+
+export default SideNav;
